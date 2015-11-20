5a38f4c18fdbba6284d46fa2db2ccc9c1e76751d
Bernd Strebel <b.strebel@digitec.de>
2015-11-20 17:11:26 +0100

    snapshot

--- tvlog.py
+++ tvlog.py
@@ -5,11 +5,12 @@
     filter tv shows and episodes at TheTVDB
 """
 
+from __future__ import absolute_import
+from __future__ import print_function
+
 __version__ = "1.0"
 __author__ = 'b.strebel@ebs-foto.de'
 
-from __future__ import absolute_import
-from __future__ import print_function
 import sys, os, json, codecs, time, re, inspect, csv, logging, logging.config, pprint
 # from six.moves import filter
 
